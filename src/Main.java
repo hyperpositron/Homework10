f37e8ad257f7d3fedc9a4ae0bd55b4be824fa874
Александр <hyperpositron@gmail.com>
2023-01-27 13:15:30 +0200

    task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+    }
+
+    public static void task1() {
+        System.out.println("_______________________________________Задача №1________________________________________");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        System.out.println("ФИО сотрудника — " + lastName + " " + firstName + " " + middleName);
     }
 }
